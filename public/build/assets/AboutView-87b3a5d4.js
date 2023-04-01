0691d43cae850051382f54c56931fe61552d3ddb
reavywilldan <reavywilldan@gmail.com>
2023-04-01 07:44:39 +0700

    npm run build

---
+++ public/build/assets/AboutView-87b3a5d4.js
@@ -0,0 +1,1 @@
+import{_ as e,o,j as s,l as t}from"./_plugin-vue_export-helper-73967a81.js";const c={},a={class:"about"},n=t("h1",null,"This is an about page",-1),_=[n];function r(i,d){return o(),s("div",a,_)}const u=e(c,[["render",r]]);export{u as default};
